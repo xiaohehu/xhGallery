07612bd3d980c9ac7400c2026f5e552c3a727796
xiaohehu <xiaohe.hu@sarah_macbook.neoscape.local>
2014-12-24 11:07:14 -0500

    Initial Commit

---
+++ XHGallery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XHGallery
+//
+//  Created by Xiaohe Hu on 12/24/14.
+//  Copyright (c) 2014 Neoscape. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XHGallery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XHGallery
+//
+//  Created by Xiaohe Hu on 12/24/14.
+//  Copyright (c) 2014 Neoscape. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
